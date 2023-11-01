b31ee98a90e658bc4d2e24cef37eef59fcf74de5
CHAKIR Mohammed Amine <predator.corp@proton.me>
2023-11-01 21:20:20 +0100

    DELETED PREVIOUS VERSION TO AVOID ERRORS AND VERSION COLLISIONS

--- src/rooms/schema/Player.ts
+++
@@ -1,7 +0,0 @@
-import { Schema, Context, type } from "@colyseus/schema";
-
-export class Player extends Schema
-{
-    @type("number") score: number = 0;
-    @type("String") username: String;
-}
--- src/rooms/schema/State.ts
+++
@@ -1,7 +0,0 @@
-import { Schema, MapSchema, type } from "@colyseus/schema";
-import {Player} from "./Player";
-
-export class State extends Schema
-{
-  @type({map: Player}) players = new MapSchema<Player>();
-}

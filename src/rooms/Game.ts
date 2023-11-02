37aa0f47a4b39506622a907c2af9fd4f03b230a7
root <predator.corp@proton.me>
2023-11-02 16:16:35 +0100

    Matching players with teams Setting up teams and players Initialising game state

--- src/rooms/Game.ts
+++ src/rooms/Game.ts
@@ -1,17 +1,22 @@
 import { Room, Client } from "@colyseus/core";
 import { Schema, Context, type, MapSchema } from "@colyseus/schema";
 
-export enum TEAM
-{
-  BLUE,
-  RED
-}
+const crypto = require("crypto")
 export class Player extends Schema
 {
+
+  constructor(username: any, id: any, team: number)
+  {
+    super();
+    this.id = id;
+    this.username= username
+    this.team = team;
+  }
+
   @type("string") username: string;
   @type("string") id: string;
 
-  @type(TEAM) team: TEAM;
+  @type("number") team: number;
 
   @type(["number"]) hand: number[] = [];
   @type("number") chosenCard: number = -1;
@@ -21,36 +26,63 @@ export class State extends Schema
   @type({ map: Player }) teamBlue = new MapSchema<Player>();
   @type({ map: Player }) teamRed = new MapSchema<Player>();
 
-  @type(["number"]) deck: number[] = [];
+  @type(["string"]) deck: String[] = [
+    "10H", "10D", "10C", "10S",
+    "7H", "7D", "7C", "7S",
+    "8H", "8D", "8C", "8S",
+    "9H", "9D", "9C", "9S",
+    "JH", "JD", "JC", "JS",
+    "QH", "QD", "QC", "QS",
+    "KH", "KD", "KC", "KS",
+    "AH", "AD", "AC", "AS"];
 
   @type("number") teamBlueScore: number = 0;
   @type("number") teamRedScore: number = 0;
+
 }
 
 export class Game extends Room<State>
 {
   maxClients = 4;
 
-  onCreate (options: any) {
+  onCreate (options: any)
+  {
+    this.roomId = crypto.createHash("sha256").update(String(Math.random())).digest('hex');
     this.setState(new State());
-
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
-    });
+    this.onMessage("action", (client, message) => this.playerAction(client, message));
   }
 
-  onJoin (client: Client, options: any) {
-    console.log(client.sessionId, "joined!");
+  onJoin (client: Client, options: any)
+  {
+    if (this.state.teamBlue.size !== 2)
+      this.state.teamBlue.set(client.sessionId, new Player(options.username, client.sessionId, 0));
+    else
+      this.state.teamRed.set(client.sessionId, new Player(options.username, client.sessionId, 1));
+    console.log(options.username + '@' + client.sessionId, "joined!");
   }
 
-  onLeave (client: Client, consented: boolean) {
+  onLeave (client: Client, consented: boolean)
+  {
     console.log(client.sessionId, "left!");
   }
 
-  onDispose() {
+  onDispose()
+  {
     console.log("room", this.roomId, "disposing...");
   }
 
+  playerAction(client: Client, data: any)
+  {
+
+  }
+
+  shuffle(array: Array<String>)
+  {
+    for (let i = array.length - 1; i > 0; i--)
+    {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 }

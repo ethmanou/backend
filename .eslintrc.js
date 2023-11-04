5424963f730947ac4c9088770adf9c9bd7a5606b
root <predator.corp@proton.me>
2023-11-04 13:24:19 +0100

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
     "overrides": [
         {
             "env": {
@@ -23,7 +18,8 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "plugins": [
         "@typescript-eslint"
